cf32bc6ae9d5005b68b1c5a7a3769e4d32156e15
Trofim-tsk <m.trofimov1987@gmail.com>
2020-11-25 22:15:22 +0700

    CSS-JS-clock complete

--- main.js
+++ main.js
@@ -2,20 +2,20 @@ const secondHand = document.querySelector(`.second-hand`);
 const minuteHand = document.querySelector(`.min-hand`);
 const hourHand = document.querySelector(`.hour-hand`);
 
-function setDate() {
+function updateHands() {
   const now = new Date();
 
   const hours = now.getHours();
-  const hourDegrees = (hours / 12 * 360) + 90;
-  hourHand.style.transform = `rotate(${hourDegrees}deg`;
+  const hourHandAngle = (hours / 12 * 360) + 90;
+  hourHand.style.transform = `rotate(${hourHandAngle}deg`;
 
   const minutes = now.getMinutes();
-  const minuteDegrees = (minutes / 60 * 360) + 90;
-  minuteHand.style.transform = `rotate(${minuteDegrees}deg`;
+  const minuteHandAngle = (minutes / 60 * 360) + 90;
+  minuteHand.style.transform = `rotate(${minuteHandAngle}deg`;
 
   const seconds = now.getSeconds(); 
-  const secondDegrees = (seconds / 60 * 360) + 90; 
-  secondHand.style.transform = `rotate(${secondDegrees}deg`;
+  const secondHandAngle = (seconds / 60 * 360) + 90; 
+  secondHand.style.transform = `rotate(${secondHandAngle}deg`;
   };
 
-setInterval(setDate, 1000);
+setInterval(updateHands, 1000);
